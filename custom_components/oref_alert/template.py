1c1cc080b6ca4d450232adc2b02187ed0622246f
root <root@homeassistant.local.hass.io>
2025-05-24 04:00:00 +0300

    config files on 24-05-2025 04:00:00

--- custom_components/oref_alert/template.py
+++ custom_components/oref_alert/template.py
@@ -6,8 +6,13 @@
 from typing import Any
 
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.template import Template, TemplateEnvironment
-from homeassistant.util.location import vincenty
+from homeassistant.helpers.template import (
+    Template,
+    TemplateEnvironment,
+)
+from homeassistant.helpers.template import (
+    distance as distance_func,
+)
 
 from custom_components.oref_alert.categories import category_to_emoji, category_to_icon
 from custom_components.oref_alert.metadata.area_info import AREA_INFO
@@ -29,22 +34,16 @@ def inject_template_extensions(hass: HomeAssistant) -> None:
         """Convert area to district."""
         return AREA_TO_DISTRICT.get(area, area)
 
-    def area_to_distance(area: str) -> float:
-        """Calculate distance of area from home coordinates."""
+    def area_to_distance(area: str, *args: Any) -> float | None:
+        """Calculate distance of area from home or provided coordinates."""
         if (area_info := AREA_INFO.get(area)) is None:
-            return -1
-        return round(
-            vincenty(
-                (hass.config.latitude, hass.config.longitude),
-                (area_info["lat"], area_info["long"]),
-            )
-            or -1,
-            1,
-        )
+            return None
+        return distance_func(hass, area_info["lat"], area_info["long"], *args)
 
-    def area_distance_test(area: str, distance: float) -> bool:
-        """Check if area is within the distance from home coordinates."""
-        return 0 <= area_to_distance(area) <= distance
+    def area_distance_test(area: str, distance: float, *args: Any) -> bool:
+        """Check if area is within the distance from home or provided coordinates."""
+        actual = area_to_distance(area, *args)
+        return actual is not None and actual <= distance
 
     original_template_environment_init = TemplateEnvironment.__init__
 
