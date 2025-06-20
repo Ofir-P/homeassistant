62448a4ceca7e44fa901354ca3a86efef6e7d344
root <root@homeassistant.local.hass.io>
2025-06-20 04:00:00 +0300

    config files on 20-06-2025 04:00:00

--- custom_components/oref_alert/coordinator.py
+++ custom_components/oref_alert/coordinator.py
@@ -23,6 +23,7 @@ from .const import (
     ATTR_CATEGORY,
     ATTR_TITLE,
     CONF_ALERT_ACTIVE_DURATION,
+    CONF_ALL_ALERTS_ATTRIBUTES,
     CONF_AREA,
     CONF_DURATION,
     CONF_POLL_INTERVAL,
@@ -68,15 +69,8 @@ class OrefAlertCoordinatorData:
         active_alerts = OrefAlertDataUpdateCoordinator.recent_alerts(
             items, active_duration
         )
-        updates = list(filter(lambda alert: _is_update(alert), active_alerts))
         self.active_alerts = list(filter(lambda alert: _is_alert(alert), active_alerts))
-        active_alerts_areas = {alert["data"] for alert in self.active_alerts}
-        self.updates = list(
-            filter(
-                lambda alert: alert["data"] not in active_alerts_areas,
-                updates,
-            )
-        )
+        self.updates = list(filter(lambda alert: _is_update(alert), active_alerts))
 
 
 def _sort_alerts(item1: dict[str, Any], item2: dict[str, Any]) -> int:
@@ -111,6 +105,9 @@ class OrefAlertDataUpdateCoordinator(DataUpdateCoordinator[OrefAlertCoordinatorD
         self._active_duration = config_entry.options.get(
             CONF_ALERT_ACTIVE_DURATION, DEFAULT_ALERT_ACTIVE_DURATION
         )
+        self._all_alerts: bool = config_entry.options.get(
+            CONF_ALL_ALERTS_ATTRIBUTES, False
+        )
         self._http_client = async_get_clientsession(hass)
         self._http_cache = {}
         self._synthetic_alerts: list[tuple[float, dict[str, Any]]] = []
@@ -130,6 +127,10 @@ class OrefAlertDataUpdateCoordinator(DataUpdateCoordinator[OrefAlertCoordinatorD
             or self._synthetic_alerts
         ):
             history = self._fix_areas_spelling(history) if history else []
+            if not self._all_alerts:
+                history = OrefAlertDataUpdateCoordinator.recent_alerts(
+                    history, self._active_duration
+                )
             alerts = (
                 self._current_to_history_format(current, history) if current else []
             )
