a996cf032436a677f17d72524ebf91139c8dc0ac
root <root@homeassistant.local.hass.io>
2026-03-07 04:00:00 +0200

    config files on 07-03-2026 04:00:00

--- custom_components/oref_alert/coordinator.py
+++ custom_components/oref_alert/coordinator.py
@@ -123,10 +123,13 @@ class OrefAlertDataUpdateCoordinator(DataUpdateCoordinator[OrefAlertCoordinatorD
     async def async_save(self) -> None:
         """Persist current areas to storage as raw records."""
         if not self._first_update:
+            cutoff = dt_util.now() - timedelta(days=1)
             await self._store.async_save(
                 {
                     CONF_AREAS: {
-                        area: record.raw_dict for area, record in self._areas.items()
+                        area: record.raw_dict
+                        for area, record in self._areas.items()
+                        if record.time > cutoff
                     }
                 }
             )
@@ -204,7 +207,7 @@ class OrefAlertDataUpdateCoordinator(DataUpdateCoordinator[OrefAlertCoordinatorD
                 self._get_synthetic_alerts(),
                 self._process_history_alerts(history or [], self._history_to_record),
                 self._process_history_alerts(history2 or [], self._history2_to_record),
-                self._current_to_history_format(current),
+                self._current_to_history_format(current if current_modified else None),
                 itertools.chain.from_iterable(
                     channel.items() for channel in self._channels
                 ),
